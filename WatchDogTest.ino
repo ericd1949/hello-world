fb52c71eb3de06d03602ed7df8d7108f81eebb3b
ericd1949 <eric@sixfeetfour.com>
2016-12-28 15:59:26 +0000

    Add files via upload

---
+++ WatchDogTest.ino
@@ -0,0 +1,15 @@
+#include <avr/wdt.h>
+void setup()
+{
+	wdt_disable();  //disable previous watchdog
+	Serial.begin(9600);
+	Serial.print("hello");
+	wdt_enable (WDTO_8S);
+	Serial.println(" Watch dog enabled");
+}
+void loop()
+{
+	Serial.print(".");
+	delay(500);
+	//wdt_reset();
+}
